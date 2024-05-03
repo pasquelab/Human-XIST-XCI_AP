38e1fda3b81a8045045bdf93ddc5a5796fda9286
jeannebrouillet <146822553+jeannebrouillet@users.noreply.github.com>
2024-05-03 11:16:02 +0200

    Add files via upload

---
+++ SCRIPT/Fig_2/raw-data_pre-processing/CUT&RUN_and_ChIPseq/A-de-Sousa-2023_capacitated-data/merging_rep_bams.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+################################ Slurm options #################################
+
+### Job name
+#SBATCH --job-name=Merging_rep
+
+### Output
+#SBATCH --output=Merging_rep-%j.out  # both STDOUT and STDERR
+
+################################   Modules    #################################
+module load samtools/1.13
+
+################################   Workflow    #################################
+
+input_path=/shared/projects/tsc/cutrun_alldata_test/Agostinho-de-Sousa-2023_capacitated-data/bam/
+output_path=/shared/projects/tsc/cutrun_alldata_test/Agostinho-de-Sousa-2023_capacitated-data/bam/merged_downsampled_bam/
+
+
+if [ ! -d "${output_path}" ]; then
+    mkdir -p "${output_path}"
+fi
+
+
+samtools merge -o ${output_path}H9-d0_H3K27me3_Filtered.merged.bam ${input_path}SRR22367278_Filtered.bam ${input_path}SRR22367277_Filtered.bam
+samtools merge -o ${output_path}H9-d0_H3K9me3_Filtered.merged.bam ${input_path}SRR22367282_Filtered.bam ${input_path}SRR22367281_Filtered.bam
+
+samtools merge -o ${output_path}H9-d10_H3K27me3_Filtered.merged.bam ${input_path}SRR22367276_Filtered.bam ${input_path}SRR22367275_Filtered.bam
+samtools merge -o ${output_path}H9-d10_H3K9me3_Filtered.merged.bam ${input_path}SRR22367280_Filtered.bam ${input_path}SRR22367279_Filtered.bam
+
+
