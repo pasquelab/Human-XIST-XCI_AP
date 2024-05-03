4dedab4b18b709a5cfb28b698f2b3c54975e19b0
jeannebrouillet <146822553+jeannebrouillet@users.noreply.github.com>
2024-05-03 11:18:32 +0200

    Add files via upload

---
+++ SCRIPT/Fig_2/raw-data_pre-processing/CUT&RUN_and_ChIPseq/Naive_Primed_TSC_EXMC/1b-alignment-bed-Naive/exeSnakemakeSlurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+################################ Slurm options #################################
+
+### Job name
+#SBATCH --job-name=Mapping
+
+### Output
+#SBATCH --output=Mapping-%j.out  # both STDOUT and STDERR
+
+################################   Modules    #################################
+
+module load snakemake/7.32.4
+module list
+
+################################   Workflow    #################################
+
+snakemake --unlock
+snakemake -krp -j 200 --resources load=100 --cluster "sbatch -p {cluster.partition} --cpus-per-task {cluster.cpu} --mem {cluster.ram} --job-name {rule}" --cluster-config cluster_config.json --latency-wait 1800 --max-jobs-per-second 1 --configfile config.json --use-singularity
+# snakemake -n : to perform a dry run 
